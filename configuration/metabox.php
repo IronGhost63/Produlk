98d06a0f5454338024bfbd9e6c7b37524f230b6b
Jirayu Yingthawornsuk <me@jirayu.in.th>
2016-04-29 21:48:03 +0700

    fix invalid register key

--- configuration/metabox.php
+++ configuration/metabox.php
@@ -4,4 +4,25 @@
  *
  * @package Produlk
  */
+ 
+ add_action( 'cmb2_admin_init', 'produlk_basic_detail' );
+ 
+ function produlk_basic_detail(){
+	 $prefix = "produlk_";
+	 
+	 $metabox = new_cmb2_box(array(
+		 'id' => $prefix . 'box_basic',
+		 'title' => __('Basic details', 'produlk'),
+		 'object_types' => array('produlk'),
+		 'context' => 'normal',
+		 'priority' => 'high',
+		 'show_name' => true
+ 	 ));
+	  
+	  $metabox->add_field(array(
+		  'name' => __('Price', 'produlk'),
+		  'id' => $prefix . 'price',
+		  'type' => 'text'
+	  ));
+ }
 ?>
