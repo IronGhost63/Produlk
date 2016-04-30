dcd9e6618ae3aa9242b6b5c037c70f5b57331a90
Jirayu Yingthawornsuk <me@jirayu.in.th>
2016-04-30 10:34:51 +0700

    Add product option meta box

--- configuration/metabox.php
+++ configuration/metabox.php
@@ -24,5 +24,31 @@
 		  'id' => $prefix . 'price',
 		  'type' => 'text'
 	  ));
+	  
+	  $productOptions = $metabox->add_field(array(
+		  'name' => __('Available Options', 'produlk'),
+		  'id' => $prefix . 'options',
+		  'type' => 'group',
+		  'description' => __('Specify options for this product, like available color, size, or quantity', 'produlk'),
+		  'options' => array(
+			  'group_title' => __('Product Options', 'produlk'),
+			  'add_button' => __('Add New Option', 'produlk'),
+			  'remove_button' => __('Remove Option', 'produlk'),
+			  'sortable' => true
+		  )
+	  ));
+	  
+	  $metabox->add_group_field($productOptions, array(
+		  'name' => __('Option', 'produlk'),
+		  'id' => $prefix . 'option_name',
+		  'type' => 'text'
+	  ));
+	  
+	  $metabox->add_group_field($productOptions, array(
+		  'name' => __('Available Values', 'produlk'),
+		  'id' => $prefix . 'option_value',
+		  'type' => 'textarea_small',
+		  'description' => 'A value per line'
+	  ));
  }
 ?>
